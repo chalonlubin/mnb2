dd965584591c71e0c91f4646c063031d9476e977
Chalon Lubin <chalonlubin1@gmail.com>
2023-05-24 22:11:18 -0700

    Update to footer

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Layout from "../components/layout";
+import { TextField, Button } from "@mui/material";
 
 export default function Home() {
   return (
@@ -11,11 +12,39 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        <main>
-          <div>
-
+        <main className="container">
+          <div className="hero">
             <h1>Welcome to My Neighbor's Bookshelf </h1>
+            <h2>Share your book collection, locally!</h2>
           </div>
+
+          <div className="interaction-container">
+            <div className="book-container">
+              <label htmlFor="book-zip" name="book">
+                Find books in your area
+              </label>
+              <TextField
+                id="outlined-basic"
+                label="Address or Zip-code"
+                variant="outlined"
+              />
+              <Button variant="contained">Search</Button>
+              <label htmlFor="book-input" name="outlined-basic">
+                Search for a book near you
+              </label>
+              <TextField
+                id="outlined-basic"
+                label="Book Title"
+                variant="outlined"
+              />
+              <Button variant="contained">Search</Button>
+            </div>
+            <div className="map-container">
+              <p>Map with pins of books here</p>
+            </div>
+          </div>
+
+          <div>Book Information Here when selected</div>
         </main>
       </Layout>
     </>
