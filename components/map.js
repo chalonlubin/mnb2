21d7b4908401cb63da6237d7fae1355a38ab4675
Chalon Lubin <chalonlubin1@gmail.com>
2023-06-04 13:51:08 -0700

    Fix needed for broken JSX

--- components/map.js
+++ components/map.js
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect, useState } from "react";
 import mapboxgl from "!mapbox-gl";
+import ReactDom from "react-dom"
 
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_API_KEY;
 
@@ -9,6 +10,43 @@ export default function Map() {
   const [lng, setLng] = useState(-70.9);
   const [lat, setLat] = useState(42.35);
   const [zoom, setZoom] = useState(9);
+
+  const geojson = {
+    type: "FeatureCollection",
+    features: [
+      {
+        type: "Feature",
+        geometry: {
+          type: "Point",
+          coordinates: [-77.032, 38.913],
+        },
+        properties: {
+          title: "Mapbox",
+          description: "Washington, D.C.",
+        },
+      },
+      {
+        type: "Feature",
+        geometry: {
+          type: "Point",
+          coordinates: [-122.414, 37.776],
+        },
+        properties: {
+          title: "Mapbox",
+          description: "San Francisco, California",
+        },
+      },
+    ],
+  };
+
+  // add markers to map
+  for (const feature of geojson.features) {
+    // create a HTML element for each feature
+    const el = ReactDom.render(<div className="marker"/>)
+
+    // make a marker for each feature and add to the map
+    new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(map);
+  }
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
